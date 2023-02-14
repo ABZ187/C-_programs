c5108621b94317b27065828183cd40cd8edbaf9f
ABZ187 <arbaazshaikh187@gmail.com>
2023-02-14 19:56:54 +0530

    first3 commit

--- storage_class_extern2.cpp
+++ storage_class_extern2.cpp
@@ -4,8 +4,7 @@ using namespace std;
 extern void x_square(int x);
 extern int ext; // extern type run first storage_class_extern.cpp
 int main(){
-    int i; // auto type
-    register int j; // register type 
+
     x_square(ext);
     return 0;
 }
